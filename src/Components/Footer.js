457de5d3cfc2436bf519fba95d7b6fc1493b06eb
Ashutosh Parab <71208674+Ashutosh-cyber@users.noreply.github.com>
2022-10-29 00:15:01 +0530

    commit4

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,11 +3,12 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Link } from "@mui/material";
 import "../Styles/Footer.css";
 
 const socialMap = {
   twitter: "https://twitter.com/",
+  instagram: "https://instagram.com/",
+  facebook: "https://www.facebook.com/",
 };
 
 function Footer() {
@@ -34,11 +35,10 @@ function Footer() {
           <h2>Share us on</h2>
         </div>
         <div className="socialMedia">
-          <Link href="#https://www.instagram.com/">
-            <InstagramIcon />
-          </Link>
-          <TwitterIcon onClick={() => openSocialLink("twitter")} />{" "}
-          <FacebookIcon /> <LinkedInIcon />
+          <InstagramIcon onClick={() => openSocialLink("instagram")} />
+
+          <TwitterIcon onClick={() => openSocialLink("twitter")} />
+          <FacebookIcon onClick={() => openSocialLink("facebook")} />
         </div>
         <p> &copy; 2022 ashutoshfoods.com</p>
       </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,56 +2,54 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import BannerImage from "../assets/background.jpg";
 import "../Styles/Home.css";
-import Carousel from 'react-bootstrap/Carousel';
 
 function home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+    <div className="home">
       <div className="headerContainer">
         <h1> ASHUTOSH FOODS </h1>
         <p> DRY FRUITS ,SPICES AND GRAINS WHOLESALERS </p>
       </div>
 
-    <Carousel className="home-carousel">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/200/300"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/200/300"
-          alt="Second slide"
-        />
+      <Carousel className="home-carousel">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://cdn.pixabay.com/photo/2017/06/29/16/58/spices-2454799_960_720.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/4663476/pexels-photo-4663476.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="Second slide"
+          />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/200/300"
-          alt="Third slide"
-        />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/200/300"
+            alt="Third slide"
+          />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
